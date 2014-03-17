a0dba13f31b0bd3c438f09c6669fcd7d2d7f5d81
AllyReid <support@dojodigi.com>
2014-03-17 15:32:33 +0000

    updated header.php

--- themes/c5f5boilerplate/elements/header.php
+++ themes/c5f5boilerplate/elements/header.php
@@ -1,4 +1,18 @@
 <?php   defined('C5_EXECUTE') or die("Access Denied."); ?>
+
+<?php  defined('C5_EXECUTE') or die("Access Denied.");
+Loader::library('3rdparty/mobile_detect');
+$md = new Mobile_Detect();
+$deviceType = 'desktop';
+if ($md->isTablet()) {
+    $deviceType = 'tablet';
+}
+else if ($md->isMobile()) {
+    $deviceType = 'mobile';
+}
+define("DEVICE_TYPE", $deviceType);
+?>
+
 <!DOCTYPE html>
 <html lang="<?php echo LANGUAGE?>">
 <head>
@@ -12,11 +26,11 @@
         <style type="text/css">#ccm-highlighter, .ccm-menu {margin-top:-49px;}</style>
     <?php } ?>
 </head>
-<body>
+<body class="<?php if ($c->isEditMode()) { ?>edit <?php } ?><?php $pageTypeName = strtolower($c->getCollectionTypeName());  $bodyClass = str_replace(' ', '-', trim($pageTypeName)); echo $bodyClass . ' ' . $deviceType; ?> clearfix ">
 
 <div class="row">
     <div class="small-12 columns">
-        <?php 
+        <?php
             $a = new GlobalArea('Header Nav');
             $a->display();
         ?>
